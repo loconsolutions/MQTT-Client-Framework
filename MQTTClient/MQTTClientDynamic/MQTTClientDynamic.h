4c96dc8b9bd7ce38d354720054df6d7022e0fbc1
Ankit Aggarwal <ankit.spd@gmail.com>
2015-10-30 23:41:39 +0530

    Add Dynamic Framework Target

---
+++ MQTTClient/MQTTClientDynamic/MQTTClientDynamic.h
@@ -0,0 +1,30 @@
+//
+//  MQTTClientDynamic.h
+//  MQTTClientDynamic
+//
+//  Created by Ankit Aggarwal on 30/10/15.
+//  Copyright © 2015 Christoph Krey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MQTTClientDynamic.
+FOUNDATION_EXPORT double MQTTClientDynamicVersionNumber;
+
+//! Project version string for MQTTClientDynamic.
+FOUNDATION_EXPORT const unsigned char MQTTClientDynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MQTTClientDynamic/PublicHeader.h>
+
+/**
+ Include this file to use MQTTClient classes in your application
+ 
+ @author Christoph Krey krey.christoph@gmail.com
+ @see http://mqtt.org
+ */
+
+#import <Foundation/Foundation.h>
+
+#import <MQTTClientDynamic/MQTTSession.h>
+#import <MQTTClientDynamic/MQTTMessage.h>
+#import <MQTTClientDynamic/MQTTSSLSecurityPolicy.h>
